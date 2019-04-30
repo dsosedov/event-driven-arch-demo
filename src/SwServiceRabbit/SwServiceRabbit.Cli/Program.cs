ffbf0aa6be6a8391de10a22c3e971a4ecaedb151
Dmitrii Sosedov <d.sosedov@gmail.com>
2019-04-30 00:33:11 -0400

    Finish retry logic

--- src/SwServiceRabbit/SwServiceRabbit.Cli/Program.cs
+++ src/SwServiceRabbit/SwServiceRabbit.Cli/Program.cs
@@ -2,6 +2,7 @@
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
+using System.Net.Http;
 using System.Text;
 
 namespace SwServiceRabbit.Cli
@@ -10,21 +11,23 @@ namespace SwServiceRabbit.Cli
     {
         const string QueueName = "sw";
 
+        static IModel _channel;
+
         static void Main(string[] args)
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
 
             using (var connection = factory.CreateConnection(nameof(SwServiceRabbit)))
             {
-                using (var channel = connection.CreateModel())
+                using (_channel = connection.CreateModel())
                 {
-                    channel.QueueDeclare(QueueName, false, false, false, null);
+                    _channel.QueueDeclare(QueueName, false, false, false, null);
 
-                    var consumer = new EventingBasicConsumer(channel);
+                    var consumer = new EventingBasicConsumer(_channel);
 
                     consumer.Received += (model, eventArgs) => HandleReceivedMessage(model, eventArgs);
 
-                    channel.BasicConsume(QueueName, true, consumer);
+                    _channel.BasicConsume(QueueName, true, consumer);
 
                     Console.ReadLine();
                 }
@@ -37,13 +40,47 @@ namespace SwServiceRabbit.Cli
             var message = Encoding.UTF8.GetString(body);
             var error = string.Empty;
 
+            Toon toon;
+
             try
             {
-                var toon = JsonConvert.DeserializeObject<Toon>(message);
+                toon = JsonConvert.DeserializeObject<Toon>(message);
             }
             catch (Exception ex)
             {
                 error = "- could not parse the message!";
+
+                Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}: [{args.RoutingKey}] {message}{error}");
+
+                return;
+            }
+
+            try
+            {
+                var url = string.Empty;
+
+                switch (toon.Order)
+                {
+                    case Order.Jedi:
+                        url = "http://localhost:51585/api/jedis";
+                        break;
+                    case Order.Sith:
+                        url = "http://localhost:51586/api/siths";
+                        break;
+                }
+
+                var client = new HttpClient();
+
+                var response = client.PostAsync(url, new StringContent($"{{name:\"{toon.Name}\"}}", Encoding.UTF8, "application/json")).Result;
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+            catch (Exception ex)
+            {
+                _channel.BasicPublish("", QueueName, null, body);
             }
 
             Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}: [{args.RoutingKey}] {message}{error}");
